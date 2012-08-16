c926966d88d8cf48ab4a972642d1110b49223d59
Tijs Verkoyen <tijs@verkoyen.eu>
2012-08-16 16:37:32 +0200

    Trying to show a form

--- creative_comments_content.js
+++ creative_comments_content.js
@@ -43,13 +43,54 @@ creativeCommentsContent = {
 		// @todo    submit the form
 	},
 
+	removeForm: function()
+	{
+		// remove the form if it already exists
+		var $creativeCommentsHolder = document.getElementById('creativeCommentsHolder');
+		if($creativeCommentsHolder != null) document.removeChild(document.getElementById('creativeCommentsHolder'));
+	},
+
 	showForm: function(id)
 	{
-		var message = "Bekijk mijn volledige reactie via: http://www.foobar.be, [typ iets anders werkt het niet]";
+		// remove previous
+		creativeCommentsContent.removeForm();
 
+		// build html
+		var html = '<div id="creativeCommentsHolder">'+
+		           '    <h1>Creative Comments</h1>' +
+		           '    <form method="POST" name="creativeCommentsForm" id="creativeCommentsForm" onsubmit="submitForm()">' +
+		           '        <p>' +
+		           '            <label for="text">Tekst</label>' +
+		           '            <textarea name="text" id="text"></textarea>' +
+		           '        </p>' +
+		           '        <p>' +
+		           '            <input type="submit" value="save" />' +
+		           '        </p>' +
+		           '    </form>' +
+		           '</div>';
+
+		// append the HTML
+		document.body.innerHTML += html;
+
+		// init some vars
+		var $creativeCommentsHolder = document.getElementById('creativeCommentsHolder');
+		var $creativeCommentsForm = document.getElementById('creativeCommentsForm');
+
+		//var message = "Bekijk mijn volledige reactie via: http://www.foobar.be, [typ iets anders werkt het niet]";
 		// set content @remark for some reason you have to type something otherwise you can't submit the form
-		creativeCommentsContent.setContent(id, message);
+		//creativeCommentsContent.setContent(id, message);
+	},
+
+	submitForm: function(e) {
+		console.log('juij...');
+		return false;
 	}
 }
 
 creativeCommentsContent.init();
+
+function submitForm()
+{
+	alert('VIEZE HOER');
+	return false;
+}
