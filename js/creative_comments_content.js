7b45802603abb31022261f23679de6d251a9cfc8
Tijs Verkoyen <tijs@verkoyen.eu>
2012-12-28 08:57:45 +0100

    Use overflow to show an scrollbar.
    Facebook limits the height of the body so the form isn't show fully.
    This is a quick workaround.

--- js/creative_comments_content.js
+++ js/creative_comments_content.js
@@ -292,6 +292,9 @@ creativeCommentsContent =
 		var $creativeCommentsHolder = $('#creativeCommentsHolder');
 		var $creativeCommentsForm = $('#creativeCommentsForm');
 
+		// reset height, because Facebook limits the height
+		$creativeCommentsForm.css('height', $('body').height() + 'px');
+
 		// bind events
 		$creativeCommentsForm.on('submit', creativeCommentsContent.submitForm);
 		$('#creativeCommentsForm #videoRecorderRecordButton').on('click', creativeCommentsContent.video.startRecording);
