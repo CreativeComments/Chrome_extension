1c175400bc18dab976a089205888bd32830ef5b3
Tijs Verkoyen <tijs@verkoyen.eu>
2013-04-18 14:30:40 +0200

    I hate versioning

--- js/creative_comments_content.js
+++ js/creative_comments_content.js
@@ -6,7 +6,7 @@
  */
 creativeCommentsContent =
 {
-	version: '0.1',
+	version: '0.0.24',
 	debug: false,
 	siteUrl: 'https://beta.creativecomments.cc',
 	apiUrl: 'https://beta.creativecomments.cc/en/api/server',
