d401e17386a334357989f6f359465b15564add0b
Tijs Verkoyen <tijs@verkoyen.eu>
2013-04-18 14:19:58 +0200

    No debugging

--- js/creative_comments_content.js
+++ js/creative_comments_content.js
@@ -7,7 +7,7 @@
 creativeCommentsContent =
 {
 	version: '0.1',
-	debug: true,
+	debug: false,
 	siteUrl: 'https://beta.creativecomments.cc',
 	apiUrl: 'https://beta.creativecomments.cc/en/api/server',
 	clickedElement: null,
