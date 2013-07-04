7262a1f83400466483c19993bbfdce30e18a5ec4
Tijs Verkoyen <tijs@verkoyen.eu>
2013-07-04 15:35:27 +0200

    Bumped version

--- js/creative_comments_content.js
+++ js/creative_comments_content.js
@@ -6,7 +6,7 @@
  */
 creativeCommentsContent =
 {
-	version: '0.0.31',
+	version: '0.0.32',
 	debug: false,
 	siteUrl: 'https://beta.creativecomments.cc',
 	apiUrl: 'https://beta.creativecomments.cc/en/api/server',
