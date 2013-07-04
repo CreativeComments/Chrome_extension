07a0f3c6768207341538e9fbf02813cf9699021d
Tijs Verkoyen <tijs@verkoyen.eu>
2013-07-04 15:30:36 +0200

    Check if we are on Facebook.
    This is added to prevent people from creating CC's in the comment-box
    embedded on other sites.

--- js/creative_comments_background.js
+++ js/creative_comments_background.js
@@ -13,19 +13,26 @@ creativeCommentsBackground = {
 
 	click: function(info, tab)
 	{
-		// on click we should ask our content.js-files which item was clicked
-		chrome.tabs.sendRequest(
-			tab.id,
-			'creativeCommentsContent.getClickedItem',
-			function(data)
-			{
-				// show the form
-				chrome.tabs.executeScript(
-					tab.id,
-					{ code: 'creativeCommentsContent.openForm("'+ data.id + '")' }
-				);
-			}
-		);
+		if(tab.url.indexOf('facebook.com') < 0) {
+			alert('You can\'t create Creative Comments outside Facebook (for now).');
+		}
+
+		else
+		{
+			// on click we should ask our content.js-files which item was clicked
+			chrome.tabs.sendRequest(
+				tab.id,
+				'creativeCommentsContent.getClickedItem',
+				function(data)
+				{
+					// show the form
+					chrome.tabs.executeScript(
+						tab.id,
+						{ code: 'creativeCommentsContent.openForm("'+ data.id + '")' }
+					);
+				}
+			);
+		}
 	},
 
 	createContextMenu: function()
