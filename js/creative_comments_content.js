c830b916808cbd084302fe939c277347093bd7d5
Tijs Vindevogel <tvin@sumocoders.be>
2013-11-28 09:53:37 +0100

    bumped version

--- js/creative_comments_content.js
+++ js/creative_comments_content.js
@@ -6,8 +6,8 @@
  */
 creativeCommentsContent =
 {
-    version:        '0.0.51',
-    debug:          true,
+    version:        '0.0.52',
+    debug:          false,
     siteUrl:        'https://beta.creativecomments.cc',
     apiUrl:         'https://beta.creativecomments.cc/en/api/server',
     clickedElement: null,
