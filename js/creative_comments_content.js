8bb1d8ecec20cacad54b44f54e92bd7aeb8469de
Tijs Vindevogel <tvin@sumocoders.be>
2013-12-30 17:05:19 +0100

    version number bump

--- js/creative_comments_content.js
+++ js/creative_comments_content.js
@@ -6,7 +6,7 @@
  */
 creativeCommentsContent =
 {
-    version:        '0.0.52',
+    version:        '0.0.53',
     debug:          false,
     siteUrl:        'https://beta.creativecomments.cc',
     apiUrl:         'https://beta.creativecomments.cc/en/api/server',
